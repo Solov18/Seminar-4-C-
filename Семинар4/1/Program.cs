c07a2d8cc968b702e61966a314fb21049f49103f
Solov18 <solov18@list.ru>
2023-02-24 16:33:38 +0300

    seminar4

---
+++ Семинар4/1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int TakeConsoleInt(string str = "Введите число: ")
+{
+    int _num;
+    Console.Write(str);
+    int.TryParse(Console.ReadLine()!, out _num);
+    return _num;
+}
+
+int a = TakeConsoleInt();
+int Fact(int a)
+{
+    int n = 1;
+    for (int i = 1; i <= a; i++)
+          n *= i;
+     
+    return n;
+}    
+Console.Write($"{a} -> {Fact(a)}");
---
+++ Семинар4/задача№29/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] array = CreateArray();
+
+for (int i = 0; i < array.Length; i++)
+    Console.Write($" {array[i]} ");
+int[] CreateArray(int size = 8)
+{
+    int[] t_array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        t_array[i] = new Random().Next(0, 9);
+        
+    }
+    return t_array;
+
+}
+
