84d21fb950eed690d440c76450251f4844ce9a61
Solov18 <solov18@list.ru>
2023-02-28 22:25:47 +0300

    zadanie27

--- Семинар4/задача№27/Program.cs
+++ Семинар4/задача№27/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+
+
+
+int a;
+
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+ToDegree(a);
+
+// Функция 
+void ToDegree(int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        result += a % 10;
+        a = a / 10;
+    }
+
+    
+
+    Console.WriteLine(result);
+}
+
+
+
